5fc8a21e0552a6867a65dbee3fe524aa2dab1627
swapan <sdbit04@gmail.com>
2020-03-12 21:45:06 +0530

    exe

--- rename_sig_file/rename_sig_file.py
+++ rename_sig_file/rename_sig_file.py
@@ -1,8 +1,6 @@
 import os
 import shutil
-
-input_folder = r"D:\D_drive_BACKUP\MENTOR\yubaraj\Rename_sig\input"
-reference_file = r"D:\D_drive_BACKUP\MENTOR\yubaraj\Rename_sig\neinfo.dat"
+import argparse
 
 
 def read_neinfo(reference):
@@ -24,18 +22,30 @@ def rename_sig_file(input_folder, reference_file):
     abs_input_folder = os.path.abspath(input_folder)
     list_files = os.listdir(abs_input_folder)
     reference_map = read_neinfo(reference_file)
+    print(reference_map)
     os.chdir(abs_input_folder)
     for file in list_files:
         if file.endswith((".SIG", ".SIG.gz")) and "NE=" in file:
             print("file name is {}".format(file))
             file_name_items = file.split(".", maxsplit=1)
-            replacement = reference_map[file_name_items[0]]
-            new_file_name = "{0}.{1}".format(replacement, file_name_items[1])
-            print("{0} -vs- {1}".format(file, new_file_name))
-            shutil.move(file, new_file_name)
+            try:
+                replacement = reference_map[file_name_items[0]]
+            except KeyError:
+                print("No reference found in neinfo.dat for {0}, skipping this file {1}".format(file_name_items[0], file))
+            else:
+                new_file_name = "{0}.{1}".format(replacement, file_name_items[1])
+                print("{0} -vs- {1}".format(file, new_file_name))
+                shutil.move(file, new_file_name)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="provide input_dir, and reference file path")
+    parser.add_argument("input_folder", help="Provide input folder path as first argument")
+    parser.add_argument("reference_file", help="Please provide the path of reference file")
+    args = parser.parse_args()
+    # input_folder = r"D:\D_drive_BACKUP\MENTOR\yubaraj\Rename_sig\input"
+    # reference_file = r"D:\D_drive_BACKUP\MENTOR\yubaraj\Rename_sig\neinfo.dat"
+    input_folder = args.input_folder
+    reference_file = args.reference_file
     rename_sig_file(input_folder, reference_file)
-
 
